b92839575c3ff93fb2003ef89f1ca7e1f3a38351
walid101 <nuhankhan101@gmail.com>
2023-03-16 22:01:26 -0400

    Added Vercel json

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-7+r)re=zdfxz3wepwku=bp#sk$@j%2+ccnsdwqxjk3$jxy@!pv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
