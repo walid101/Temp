e4dd3baafb7fabbdce6978056db05eabf7d66147
walid101 <nuhankhan101@gmail.com>
2023-03-18 22:27:55 -0400

    CSRF Trusted Origins

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -28,8 +28,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-# CSRF_TRUSTED_ORIGINS = ['*']
-# # Application definition
+CSRF_TRUSTED_ORIGINS = ['*']
+# Application definition
 
 INSTALLED_APPS = [
     'django.contrib.admin',
